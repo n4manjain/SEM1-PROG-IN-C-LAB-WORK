36f6747dbe3a32dd72dfa4d099025468705f0994
n4manjain <namanj7300@gmail.com>
2026-08-23 23:24:37 +0530

    Add files via upload

---
+++ divide.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int a,b;
+printf("Enter first number: ");
+scanf("%d",&a);
+printf("Enter second number: ");
+scanf("%d",&b);
+printf("DIVISION OF TWO NUMBERS IS %d",a/b);
+return 0;
+}
