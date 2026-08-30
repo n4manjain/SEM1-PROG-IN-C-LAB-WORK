86538f52c9d3d85dab545b4cd3c29d2b5e8d4135
n4manjain <namanj7300@gmail.com>
2026-08-30 20:50:22 +0530

    Add files via upload

---
+++ 2nd.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+int a,b;
+printf("Enter first number: ");
+scanf("%d",&a);
+printf("Enter second number: ");
+scanf("%d",&b);
+printf("\n");
+
+
+printf("ADDITION OF TWO NUMBERS IS %d \n\n",a+b);
+printf("SUBTRACTION OF TWO NUMBERS IS %d \n\n",a-b);
+printf("MULTIPLICATION OF TWO NUMBERS IS %d \n\n",a*b);
+printf("DIVISION OF TWO NUMBERS IS %d \n\n",a/b);
+getch();
+return 0;
+}
